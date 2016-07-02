13f021ebe24c8ad3366cd9f0a88be3204e38a84d
Cody Brocious <cody.brocious@gmail.com>
2016-07-02 14:55:38 -0600

    Fixed replacement bug in psx frontend.  Split DagOp stuff into its own common file, to share between frontends.

--- psx.py
+++ psx.py
@@ -4,12 +4,12 @@ from pygenic import *
 import json, os, tblgen
 
 if not os.path.exists('psx.td.cache') or os.path.getmtime('psx.td') > os.path.getmtime('psx.td.cache'):
-	func.insts = tblgen.interpret('psx.td').deriving('Basefunc.inst')
+	insts = tblgen.interpret('psx.td').deriving('BaseInst')
 	ops = []
-	for name, (bases, data) in func.insts:
+	for name, (bases, data) in insts:
 		ops.append((name, bases[1], data['Opcode'][1], data['Function'][1] if 'Function' in data else None, data['Disasm'][1], dag2expr(data['Eval'][1])))
 	with file('psx.td.cache', 'w') as fp:
-		json.dump(ops, fp)
+		json.dump(ops, fp, sort_keys=True, indent=2)
 else:
 	ops = json.load(file('psx.td.cache'))
 
